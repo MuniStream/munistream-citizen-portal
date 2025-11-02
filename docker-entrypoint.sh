584b52d4d5de8c4e316a4668b91a28a55bbbdda1
Paola Villarreal <paw@paw.mx>
2025-11-01 18:47:22 -0600

    Fix runtime environment variable replacement for all config values
    - Add VITE_API_BASE_URL and other missing variables to .env placeholders
    - Update docker-entrypoint.sh to replace all environment variables
    - Ensures proper runtime configuration for multi-tenant deployments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,10 +18,17 @@ replace_env_vars() {
             sed -i "s|__VITE_API_BASE_URL__|${VITE_API_BASE_URL}|g" "$file"
             sed -i "s|__VITE_KEYCLOAK_URL__|${VITE_KEYCLOAK_URL}|g" "$file"
             sed -i "s|__VITE_KEYCLOAK_REALM__|${VITE_KEYCLOAK_REALM}|g" "$file"
-            sed -i "s|__VITE_KEYCLOAK_CLIENT_ID__|${VITE_KEYCLOAK_CLIENT_ID:-munistream-citizen}|g" "$file"
+            sed -i "s|__VITE_KEYCLOAK_CLIENT_ID__|${VITE_KEYCLOAK_CLIENT_ID}|g" "$file"
             sed -i "s|__VITE_TENANT__|${VITE_TENANT}|g" "$file"
+            sed -i "s|__VITE_TENANT_ID__|${VITE_TENANT_ID}|g" "$file"
             sed -i "s|__VITE_TENANT_NAME__|${VITE_TENANT_NAME}|g" "$file"
             sed -i "s|__VITE_ORGANIZATION__|${VITE_ORGANIZATION}|g" "$file"
+            sed -i "s|__VITE_APP_TITLE__|${VITE_APP_TITLE}|g" "$file"
+            sed -i "s|__VITE_LANGUAGE__|${VITE_LANGUAGE}|g" "$file"
+            sed -i "s|__VITE_ENABLE_NOTIFICATIONS__|${VITE_ENABLE_NOTIFICATIONS}|g" "$file"
+            sed -i "s|__VITE_ENABLE_FILE_UPLOAD__|${VITE_ENABLE_FILE_UPLOAD}|g" "$file"
+            sed -i "s|__VITE_MAX_FILE_SIZE__|${VITE_MAX_FILE_SIZE}|g" "$file"
+            sed -i "s|__VITE_ALLOWED_FILE_TYPES__|${VITE_ALLOWED_FILE_TYPES}|g" "$file"
         fi
     done
 }
